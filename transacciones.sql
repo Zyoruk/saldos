993e29e50328ead13a89c73bec9ec88c6dee3054
Zyoruk <Zyoruk@users.noreply.github.com>
2018-11-26 23:36:43 -0600

    Foreign use ID keys, not values.

--- transacciones.sql
+++ transacciones.sql
@@ -8,39 +8,38 @@ go
 
 create table saldo(
 _id int,
-numero_cuenta varchar(20),
-_limite int,
-_deuda int,
-_disponible int,
+_idCuenta int,
+_limite numeric(18,2),
+_deuda numeric(18,2),
+_disponible numeric(18,2),
 _moneda varchar(4),
 primary key(_id),
-constraint unique_cuentas_saldo unique (numero_cuenta),
-constraint fk_saldo_cuentas FOREIGN key (numero_cuenta) references cuentas(numero_cuenta))
+constraint unique_cuentas_saldo unique (_idCuenta),
+constraint fk_saldo_cuentas FOREIGN key (_idCuenta) references cuentas(_id))
 go
 
 create table tarjetas(
 _id int ,
-numero_cuenta varchar(20),
+_idCuenta int,
 numero_tarjeta varchar(20),
-nombre_prop varchar(128),
+nombre_prop varchar(60),
 primary key(_id),
-constraint unique_cuentas_tarjetas unique (numero_cuenta),
-constraint unique_tarjetas_tarjetas unique (numero_tarjeta),
-constraint fk_tarjeta_cuentas FOREIGN key (numero_cuenta) references cuentas(numero_cuenta))
+constraint unique_tarjetas_cuenta unique ( _idCuenta, numero_tarjeta),
+constraint fk_tarjeta_cuentas FOREIGN key (_idCuenta) references cuentas(_id))
 go
 
 create table movimiento(
 _id int,
-numero_cuenta varchar(20),
-_movimiento varchar(20),
+_idCuenta int,
+_idTarjeta int,
+movimiento varchar(20),
 descripcion varchar (256),
 moneda char(4),
-numero_tarjeta varchar(20),
-monto int,
+monto numeric(18,2),
 primary key(_id),
-constraint unique_cuentas_movimiento unique (numero_cuenta),
-constraint fk_movimiento_cuenta FOREIGN key (numero_cuenta) references cuentas(numero_cuenta),
-constraint fk_movimiento_tarjeta FOREIGN key (numero_tarjeta) references tarjetas(numero_tarjeta))
+constraint unique_movimiento_cuenta_tarjeta unique (_idCuenta, _idTarjeta),
+constraint fk_movimiento_cuenta FOREIGN key (_idCuenta) references cuentas(_id),
+constraint fk_movimiento_tarjeta FOREIGN key (_idTarjeta) references tarjetas(_id))
 go
 
 
